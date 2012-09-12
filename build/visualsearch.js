957bbb8da11f706300a82ef7a2655268b4d6988e
Pilaf <pfayolle@gmail.com>
2012-09-12 17:44:12 -0300

    Fixed more iOS focus issues.

--- build/visualsearch.js
+++ build/visualsearch.js
@@ -26,6 +26,9 @@
   // Sets the version for VisualSearch to be used programatically elsewhere.
   VS.VERSION = '0.2.2';
 
+  // Detect whether we're on a touch screen device
+  VS.isTouchDevice = 'ontouchstart' in window;
+
   VS.VisualSearch = function(options) {
     var defaults = {
       container   : '',
@@ -91,7 +94,7 @@ VS.ui.SearchBox = Backbone.View.extend({
     // Detect touch-screen devises and apply a different
     // event listener for those. This fixes the problem
     // with the search box not being focusable in iOS.
-    if ('ontouchstart' in window) {
+    if (VS.isTouchDevice) {
       events['mouseup .VS-search-box'] = 'maybeFocusSearch';
     } else {
       events['mousedown .VS-search-box'] = 'maybeFocusSearch';
@@ -201,9 +204,11 @@ VS.ui.SearchBox = Backbone.View.extend({
       if (view.model == model) return true;
     });
 
-    _.defer(function() {
+    if (VS.isTouchDevice) {
       facetView.enableEdit();
-    });
+    } else {
+      _.defer(function() { facetView.enableEdit(); });
+    }
   },
 
   // Changing a facet programmatically re-renders it.
@@ -570,7 +575,7 @@ VS.ui.SearchFacet = Backbone.View.extend({
   // If the value, when selected from the autocompletion menu, is different
   // than what it was, commit the facet and search for it.
   setupAutocomplete : function() {
-    this.box.autocomplete({
+    this.box.VSautocomplete({
       source    : _.bind(this.autocompleteValues, this),
       minLength : 0,
       delay     : 0,
@@ -971,7 +976,7 @@ VS.ui.SearchInput = Backbone.View.extend({
   //
   // See `addTextFacetRemainder` for explanation on how the remainder works.
   setupAutocomplete : function() {
-    this.box.autocomplete({
+    this.box.VSautocomplete({
       minLength : this.options.autocompleteMinLength,
       delay     : 50,
       autoFocus : true,
@@ -1647,6 +1652,217 @@ if ($.browser.msie && false) {
 }
 
 })();
+
+// Extend menu and autocomplete widgets
+// to play nicely with Webkit on iOS
+(function($) {
+  $.widget("ui.VSmenu", $.extend({}, $.ui.menu.prototype, {
+    _init: function() {
+      this.element.data('menu', this.element.data('VSmenu'));
+      return $.ui.menu.prototype._init.apply(this, arguments);
+    },
+
+    select: function( event ) {
+      this.options.selected(event, { item: this.active });
+    }
+  }));
+
+  $.widget("ui.VSautocomplete", $.extend({}, $.ui.autocomplete.prototype, {
+    _init: function() {
+      this.element.data('autocomplete', this.element.data('VSautocomplete'));
+      return $.ui.autocomplete.prototype._init.apply(this, arguments);
+    },
+
+    _create: function() {
+      var self = this,
+        doc = this.element[ 0 ].ownerDocument,
+        suppressKeyPress;
+
+      this.element
+        .addClass( "ui-autocomplete-input" )
+        .attr( "autocomplete", "off" )
+        // TODO verify these actually work as intended
+        .attr({
+          role: "textbox",
+          "aria-autocomplete": "list",
+          "aria-haspopup": "true"
+        })
+        .bind( "keydown.autocomplete", function( event ) {
+          if ( self.options.disabled || self.element.attr( "readonly" ) ) {
+            return;
+          }
+
+          suppressKeyPress = false;
+          var keyCode = $.ui.keyCode;
+          switch( event.keyCode ) {
+          case keyCode.PAGE_UP:
+            self._move( "previousPage", event );
+            break;
+          case keyCode.PAGE_DOWN:
+            self._move( "nextPage", event );
+            break;
+          case keyCode.UP:
+            self._move( "previous", event );
+            // prevent moving cursor to beginning of text field in some browsers
+            event.preventDefault();
+            break;
+          case keyCode.DOWN:
+            self._move( "next", event );
+            // prevent moving cursor to end of text field in some browsers
+            event.preventDefault();
+            break;
+          case keyCode.ENTER:
+          case keyCode.NUMPAD_ENTER:
+            // when menu is open and has focus
+            if ( self.menu.active ) {
+              // #6055 - Opera still allows the keypress to occur
+              // which causes forms to submit
+              suppressKeyPress = true;
+              event.preventDefault();
+            }
+            //passthrough - ENTER and TAB both select the current element
+          case keyCode.TAB:
+            if ( !self.menu.active ) {
+              return;
+            }
+            self.menu.select( event );
+            break;
+          case keyCode.ESCAPE:
+            self.element.val( self.term );
+            self.close( event );
+            break;
+          default:
+            // keypress is triggered before the input value is changed
+            clearTimeout( self.searching );
+            self.searching = setTimeout(function() {
+              // only search if the value has changed
+              if ( self.term != self.element.val() ) {
+                self.selectedItem = null;
+                self.search( null, event );
+              }
+            }, self.options.delay );
+            break;
+          }
+        })
+        .bind( "keypress.autocomplete", function( event ) {
+          if ( suppressKeyPress ) {
+            suppressKeyPress = false;
+            event.preventDefault();
+          }
+        })
+        .bind( "focus.autocomplete", function() {
+          if ( self.options.disabled ) {
+            return;
+          }
+
+          self.selectedItem = null;
+          self.previous = self.element.val();
+        })
+        .bind( "blur.autocomplete", function( event ) {
+          if ( self.options.disabled ) {
+            return;
+          }
+
+          clearTimeout( self.searching );
+          // clicks on the menu (or a button to trigger a search) will cause a blur event
+          self.closing = setTimeout(function() {
+            self.close( event );
+            self._change( event );
+          }, 150 );
+        });
+      this._initSource();
+      this.response = function() {
+        return self._response.apply( self, arguments );
+      };
+      this.menu = $( "<ul></ul>" )
+        .addClass( "ui-autocomplete" )
+        .appendTo( $( this.options.appendTo || "body", doc )[0] )
+        // prevent the close-on-blur in case of a "slow" click on the menu (long mousedown)
+        .mousedown(function( event ) {
+          // clicking on the scrollbar causes focus to shift to the body
+          // but we can't detect a mouseup or a click immediately afterward
+          // so we have to track the next mousedown and close the menu if
+          // the user clicks somewhere outside of the autocomplete
+          var menuElement = self.menu.element[ 0 ];
+          if ( !$( event.target ).closest( ".ui-menu-item" ).length ) {
+            setTimeout(function() {
+              $( document ).one( 'mousedown', function( event ) {
+                if ( event.target !== self.element[ 0 ] &&
+                  event.target !== menuElement &&
+                  !$.ui.contains( menuElement, event.target ) ) {
+                  self.close();
+                }
+              });
+            }, 1 );
+          }
+
+          // use another timeout to make sure the blur-event-handler on the input was already triggered
+          setTimeout(function() {
+            clearTimeout( self.closing );
+          }, 13);
+        })
+        .VSmenu({
+          focus: function( event, ui ) {
+            var item = ui.item.data( "item.autocomplete" );
+            if ( false !== self._trigger( "focus", event, { item: item } ) ) {
+              // use value to match what will end up in the input, if it was a key event
+              if ( /^key/.test(event.originalEvent.type) ) {
+                self.element.val( item.value );
+              }
+            }
+          },
+          selected: function( event, ui ) {
+            var item = ui.item.data( "item.autocomplete" ),
+              previous = self.previous;
+
+            // only trigger when focus was lost (click on menu)
+            if ( self.element[0] !== doc.activeElement ) {
+              self.previous = previous;
+              // #6109 - IE triggers two focus events and the second
+              // is asynchronous, so we need to reset the previous
+              // term synchronously and asynchronously :-(
+              setTimeout(function() {
+                self.previous = previous;
+                self.selectedItem = item;
+              }, 1);
+            }
+
+            //console.log(self);
+            //if ( false !== self._trigger( "select", event, { item: item } ) ) {
+            //	self.element.val( item.value );
+            //}
+            if ( false !== self.options.select(event, { item: item } ) ) {
+              self.element.val( item.value );
+            }
+
+            // reset the term after the select event
+            // this allows custom select handling to work properly
+            self.term = self.element.val();
+
+            self.close( event );
+            self.selectedItem = item;
+
+          },
+          blur: function( event, ui ) {
+            // don't set the value of the text field if it's already correct
+            // this prevents moving the cursor unnecessarily
+            if ( self.menu.element.is(":visible") &&
+              ( self.element.val() !== self.term ) ) {
+              self.element.val( self.term );
+            }
+          }
+        })
+        .zIndex( this.element.zIndex() + 1 )
+        // workaround for jQuery bug #5781 http://dev.jquery.com/ticket/5781
+        .css({ top: 0, left: 0 })
+        .hide()
+        .data( "VSmenu" );
+      if ( $.fn.bgiframe ) {
+         this.menu.element.bgiframe();
+      }
+    }
+  }));
+})(jQuery);
 
 (function() {
 
